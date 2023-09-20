d2c1e9ef3aa98930729bc347dc40ada70622bc47
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2023-09-20 14:53:06 +0200

    Disabled the function for mouse right click

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ function copyrightYear() {
 window.onload = copyrightYear();
 
 // It disable the mouse Right-Click to prevent from opening Context Menu
-function disableRightClick() {
-	document.addEventListener("contextmenu", (e) => {
-		e.preventDefault();
-	}, false);
-}
-window.onload = disableRightClick();
+// function disableRightClick() {
+// 	document.addEventListener("contextmenu", (e) => {
+// 		e.preventDefault();
+// 	}, false);
+// }
+// window.onload = disableRightClick();
 
 // It disable the keyboard Control & Function keys to prevent from opening Source Code
 // function disableShortcutKey() {
