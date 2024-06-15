7224e04c91c305784560eb57e5f1a8396f114814
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2024-06-15 14:41:16 +0200

    Implemented theme persistence using localStorage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,49 @@
-// Toggles the theme between Light and Dark mode, and updates the icon accordingly
+// Change the theme between light and dark mode and update the icon accordingly
 function switchTheme() {
   const switchModeBtn = document.getElementById("switchModeBtn");
   const bodyPage = document.body;
   
-  // Toggle dark mode in the body of the page
+  // Toggle dark mode on the body
   bodyPage.classList.toggle("dark-mode");
   
-  // Toggle icons and aria label based on the current theme
-  if (switchModeBtn.classList.contains("fa-moon")) {
+  // Update icons and aria label based on the current theme
+  if (bodyPage.classList.contains("dark-mode")) {
+    switchModeBtn.classList.remove("fa-moon");
+    switchModeBtn.classList.add("fa-sun");
+    switchModeBtn.setAttribute("aria-label", "Switch to light mode");
+    // Save dark mode theme preference
+    localStorage.setItem("theme", "dark");
+  } else {
+    switchModeBtn.classList.remove("fa-sun");
+    switchModeBtn.classList.add("fa-moon");
+    switchModeBtn.setAttribute("aria-label", "Switch to dark mode");
+    // Save light mode theme preference
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// Load the selected theme saved from localStorage
+function loadTheme() {
+  const switchModeBtn = document.getElementById("switchModeBtn");
+  const bodyPage = document.body;
+  const savedTheme = localStorage.getItem("theme");
+
+  // Update icons and aria label based on the selected theme
+  if (savedTheme === "dark") {
+    bodyPage.classList.add("dark-mode");
     switchModeBtn.classList.remove("fa-moon");
     switchModeBtn.classList.add("fa-sun");
     switchModeBtn.setAttribute("aria-label", "Switch to light mode");
   } else {
+    bodyPage.classList.remove("dark-mode");
     switchModeBtn.classList.remove("fa-sun");
     switchModeBtn.classList.add("fa-moon");
     switchModeBtn.setAttribute("aria-label", "Switch to dark mode");
   }
 }
+
+// Load the theme when the page is loaded
+window.onload = loadTheme();
 
 // It gets the actual year and then appears inside of the specific id selector
 function copyrightYear() {
